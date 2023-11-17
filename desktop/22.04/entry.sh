6c315004a936d67c66215751ae84042181a404fc
mojib <mojib.wali@tugraz.at>
2023-11-17 17:16:35 +0100

    wine: basic dep

--- desktop/22.04/entry.sh
+++ desktop/22.04/entry.sh
@@ -2,4 +2,8 @@
 
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_zone_name": "iplant"}' | tee  > /home/user/.irods/irods_environment.json
 
-exec xpra start --bind-tcp=0.0.0.0:9876 --html=on --start-child="gnome-terminal"  --exit-with-children=no --daemon=no --xvfb="/usr/bin/Xvfb +extension Composite -screen 0 1920x1080x24+32 -nolisten tcp -noreset" --file-transfer=on --pulseaudio=no --notifications=no --bell=no :100
+wget https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.3.1/npp.8.3.1.Installer.exe \
+    && wine npp.8.3.1.Installer.exe /S \
+    && rm npp.8.3.1.Installer.exe
+
+exec xpra start --bind-tcp=0.0.0.0:9876 --html=on --start-child="wine /root/.wine/drive_c/Program\ Files/Notepad++/notepad++.exe" --exit-with-children=no --daemon=no --xvfb="/usr/bin/Xvfb +extension Composite -screen 0 1920x1080x24+32 -nolisten tcp -noreset" --file-transfer=on --pulseaudio=no --notifications=no --bell=no :100
